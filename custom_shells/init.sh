4893201df2d4c1a9b4fda3af3f31c8b95c73bbba
trytoYH <trytolight@gmail.com>
2023-07-30 01:15:03 +0900

    add shell script

---
+++ custom_shells/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## init setting & download benchmark
+
+
+# Uninstall torch
+pip uninstall torch
+
+# Clone the repository
+git clone https://github.com/trytoYH/vllm_test.git
+cd vllm_test
+
+# Install the package in editable mode with a default timeout of 60 seconds
+pip install -e . --default-timeout=60
+
+# Add vllm_test directory to PYTHONPATH
+export PYTHONPATH=/workspace/vllm_test:$PYTHONPATH
+
+# Install ray
+pip install ray
+
+# Reinstall typing-extensions version 4.5.0
+pip install --force-reinstall typing-extensions==4.5.0
+
+# Download the JSON file
+wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
